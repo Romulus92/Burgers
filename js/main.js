5fbdfdd5afb1c7658b8af13e3386332b53149aa4
Roman Zaynullin <r0mulusss@yandex.ru>
2017-11-14 20:35:10 +0500

    minor tweaks in burger menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 var burgerMenu = document.getElementById('hamburger-menu--active')
 var buttonBurger = document.getElementById('hamburger-menu-link--onclick')
+var closeBurger = document.getElementById('hamburger__close')
 
 buttonBurger.addEventListener('click', function() {
     burgerMenu.classList.add('hamburger-menu__visible')
+})
+
+closeBurger.addEventListener('click', function() {
+    burgerMenu.classList.remove('hamburger-menu__visible')
 })
